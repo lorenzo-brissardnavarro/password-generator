8eb41999b6c38cc83feb11813d81ed409e4b8329
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2025-11-17 10:44:23 +0100

    feat fonctions verification exigences mdp

---
+++ generator.py
@@ -0,0 +1,51 @@
+import random
+import string
+
+alphabet_min = string.ascii_lowercase
+alphabet_maj = string.ascii_uppercase
+caracteres_speciaux = "!@#$%^&*"
+chiffres = "0123456789"
+
+def verification_min(mdp):
+    presence_min = False
+    for lettre in mdp:
+        if lettre in alphabet_min:
+            presence_min = True
+    return presence_min
+
+def verification_maj(mdp):
+    presence_maj = False
+    for lettre in mdp:
+        if lettre in alphabet_maj:
+            presence_maj = True
+    return presence_maj
+
+def verification_caracteres(mdp):
+    presence_caractere = False
+    for lettre in mdp:
+        if lettre in caracteres_speciaux:
+            presence_caractere = True
+    return presence_caractere
+
+def verification_chiffres(mdp):
+    presence_chiffre = False
+    for lettre in mdp:
+        if lettre in chiffres:
+            presence_chiffre = True
+    return presence_chiffre
+
+while True:
+    password = input("Veuillez entrer votre mot de passe : ")
+    if len(password) < 8:
+        print("\033[1;31mLe mot de passe doit contenir au moins 8 caractères\033[0m")
+    elif not verification_min(password):
+        print("\033[1;31mLe mot de passe doit contenir au moins une lettre minuscule\033[0m")
+    elif not verification_maj(password):
+        print("\033[1;31mLe mot de passe doit contenir au moins une lettre majuscule\033[0m")
+    elif not verification_chiffres(password):
+        print("\033[1;31mLe mot de passe doit contenir au moins un chiffre\033[0m")
+    elif not verification_caracteres(password):
+        print("\033[1;31mLe mot de passe doit contenir au moins un caractère spécial (!, @, #, $, %, ^, &, *)\033[0m")
+    else:
+        print("\033[1;32mVotre mot de passe est correct\033[0m")
+        break
