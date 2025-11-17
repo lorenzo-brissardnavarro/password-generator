9ecda740b68d13da524eab2b645204a29c26be7d
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2025-11-17 18:08:35 +0100

    feat comparaison doublons + tri selon nom utilisateur

--- generator.py
+++ generator.py
@@ -5,7 +5,7 @@ from hashlib import sha256
 alphabet_min = string.ascii_lowercase
 alphabet_maj = string.ascii_uppercase
 caracteres_speciaux = "!@#$%^&*"
-chiffres = "0123456789"
+chiffres = string.digits
 
 def verification(mdp, comparateur):
     presence = False
@@ -14,33 +14,37 @@ def verification(mdp, comparateur):
             presence = True
     return presence
 
-def hachage(mdp):
+def hachage(username, mdp):
     mdp_crypt = sha256(mdp.encode('utf-8')).hexdigest()
-    with open('mots_de_passe.json', 'r') as fichier:
-        datas = json.load(fichier)
-    for data in datas:
-        if data == mdp_crypt:
-            print("\033[1;31mCe mot de passe est déjà présent dans le fichier !\033[0m")
-            return
     try:
         with open('mots_de_passe.json', 'r') as fichier:
-            datas = json.load(fichier)
+            donnees = json.load(fichier)
     except FileNotFoundError:
-        datas = []
-    datas.append(mdp_crypt)
+        donnees = []
+    for element in donnees:
+        if element['username'] == username:
+            if mdp_crypt in element['mdp']:
+                print("\033[1;31mCe mot de passe est déjà présent dans le fichier !\033[0m")
+                return
+            element['mdp'].append(mdp_crypt)
+            break
+    else:
+        dico = {'username': username, 'mdp': [mdp_crypt]}
+        donnees.append(dico)
     with open('mots_de_passe.json', 'w') as fichier:
-        json.dump(datas, fichier, indent=4)
+        json.dump(donnees, fichier, indent=4)
 
-def affichage_donnees():
+def affichage_donnees(username):
     reponse = input("Voulez-vous afficher vos mots de passe hachés (oui ou non) ? ")
     if reponse.lower() == "oui":
         with open('mots_de_passe.json', 'r') as fichier:
-            datas = json.load(fichier)
-            for mdp in datas:
-                print(mdp)
-
+            donnees = json.load(fichier)
+            for element in donnees:
+                if element['username'] == username:
+                    print(element['mdp'])
 
 while True:
+    utilisateur = input("Saisissez votre nom d'utilisateur : ")
     password = input("Veuillez entrer votre mot de passe : ")
     if len(password) < 8:
         print("\033[1;31mLe mot de passe doit contenir au moins 8 caractères\033[0m")
@@ -54,6 +58,6 @@ while True:
         print("\033[1;31mLe mot de passe doit contenir au moins un caractère spécial (!, @, #, $, %, ^, &, *)\033[0m")
     else:
         print("\033[1;32mVotre mot de passe respecte les exigences\033[0m")
-        hachage(password)
-        affichage_donnees()
+        hachage(utilisateur,password)
+        affichage_donnees(utilisateur)
         break
