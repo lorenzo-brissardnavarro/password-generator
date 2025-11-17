640fa5f8135f72d84180464f8188a845e9322928
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2025-11-17 12:19:50 +0100

    refactor fonction verification

--- generator.py
+++ generator.py
@@ -7,33 +7,12 @@ alphabet_maj = string.ascii_uppercase
 caracteres_speciaux = "!@#$%^&*"
 chiffres = "0123456789"
 
-def verification_min(mdp):
-    presence_min = False
+def verification(mdp, comparateur):
+    presence = False
     for lettre in mdp:
-        if lettre in alphabet_min:
-            presence_min = True
-    return presence_min
-
-def verification_maj(mdp):
-    presence_maj = False
-    for lettre in mdp:
-        if lettre in alphabet_maj:
-            presence_maj = True
-    return presence_maj
-
-def verification_caracteres(mdp):
-    presence_caractere = False
-    for lettre in mdp:
-        if lettre in caracteres_speciaux:
-            presence_caractere = True
-    return presence_caractere
-
-def verification_chiffres(mdp):
-    presence_chiffre = False
-    for lettre in mdp:
-        if lettre in chiffres:
-            presence_chiffre = True
-    return presence_chiffre
+        if lettre in comparateur:
+            presence = True
+    return presence
 
 def hachage(mdp):
     mdp_crypt = sha256(mdp.encode('utf-8')).hexdigest()
@@ -58,13 +37,13 @@ while True:
     password = input("Veuillez entrer votre mot de passe : ")
     if len(password) < 8:
         print("\033[1;31mLe mot de passe doit contenir au moins 8 caractères\033[0m")
-    elif not verification_min(password):
+    elif not verification(password, alphabet_min):
         print("\033[1;31mLe mot de passe doit contenir au moins une lettre minuscule\033[0m")
-    elif not verification_maj(password):
+    elif not verification(password, alphabet_maj):
         print("\033[1;31mLe mot de passe doit contenir au moins une lettre majuscule\033[0m")
-    elif not verification_chiffres(password):
+    elif not verification(password, chiffres):
         print("\033[1;31mLe mot de passe doit contenir au moins un chiffre\033[0m")
-    elif not verification_caracteres(password):
+    elif not verification(password, caracteres_speciaux):
         print("\033[1;31mLe mot de passe doit contenir au moins un caractère spécial (!, @, #, $, %, ^, &, *)\033[0m")
     else:
         print("\033[1;32mVotre mot de passe est correct\033[0m")
