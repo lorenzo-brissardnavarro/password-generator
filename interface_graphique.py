9d36a90320ceef4bb96d9302a22e4e209868f30e
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2025-11-17 20:45:49 +0100

    feat input nom utilisateur + recup valeur

--- interface_graphique.py
+++ interface_graphique.py
@@ -1,13 +1,24 @@
 from tkinter import *
 from PIL import Image, ImageTk
 
+def page_utilisateur():
+    frame_accueil.pack_forget()
+    frame_utilisateur.pack(pady=260) 
+
+def page_choix():
+    nom_utilisateur = entry_utilisateur.get()
+    frame_utilisateur.pack_forget()
+    frame_choix.pack(pady=100)
+    label_choix.config(text=f"Bonjour {nom_utilisateur}")
+
 fenetre = Tk()
 fenetre.title("Password Validator")
 fenetre.geometry("940x680")
 fenetre.resizable(False, False)
-fenetre.configure(bg="black")
+fenetre.configure(bg="#C5E8FC")
 
-frame_accueil = Frame(fenetre, bg="black")
+#Page accueil
+frame_accueil = Frame(fenetre, bg="#C5E8FC")
 frame_accueil.pack(fill="both", expand=True)
 
 image_originale = Image.open("image_background.png")
@@ -16,11 +27,32 @@ bg = ImageTk.PhotoImage(image_redim)
 label_photo = Label(frame_accueil, image=bg)
 label_photo.place(x=0, y=0, relwidth=1, relheight=1)
 
+btn_start = Button(frame_accueil, text="Commencer", font=("Helvetica", 17, "bold"), bg="#0B3669", fg="white",activebackground="#D6F0FF", activeforeground="#0B3669", relief="flat", bd=0,padx=70, 
+pady=15, cursor="hand2", highlightthickness=0, command=page_utilisateur)
+btn_start.pack(side="bottom", pady=30)
+
+
+
+#Page demande nom utilisateur
+frame_utilisateur = Frame(fenetre, bg="#C5E8FC")
+
+label_utilisateur = Label(frame_utilisateur, text="Saisissez votre nom d'utilisateur :", font=("Helvetica", 27, "bold"), fg="#0B3669", bg="#C5E8FC")
+label_utilisateur.pack()
+
+entry_utilisateur = Entry(frame_utilisateur, font=("Helvetica", 20))
+entry_utilisateur.pack(pady=20)
+
+btn_utilisateur = Button(frame_utilisateur, text="Valider", font=("Helvetica", 17, "bold"), bg="#0B3669", fg="white",activebackground="#D6F0FF", activeforeground="#0B3669", relief="flat", bd=0,padx=70, 
+pady=15, cursor="hand2", highlightthickness=0, command=page_choix)
+btn_utilisateur.pack()
 
 
 
 
+#Page choix utilisateur
+frame_choix = Frame(fenetre, bg="#C5E8FC")
 
-
+label_choix = Label(frame_choix, text="", font=("Helvetica", 27, "bold"), fg="#0B3669", bg="#C5E8FC")
+label_choix.pack()
 
 fenetre.mainloop()
